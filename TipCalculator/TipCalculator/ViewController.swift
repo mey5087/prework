94638034fa67782957a0cfc46a2066eb0dd12dcb
Matthew Young <m11young@yahoo.com>
2020-11-20 12:09:44 -0500

    prework

---
+++ TipCalculator/TipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Robert Young on 11/17/20.
+//  Copyright © 2020 Matthew Young. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
